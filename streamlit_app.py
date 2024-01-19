a7cc7d837268d528d00d431987a1651925c9fb78
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2024-01-19 15:08:54 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,19 +3,27 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# Função para corrigir e completar o URL se necessário
+def corrigir_url(url):
+    if not url.startswith(('http://', 'https://')):
+        url = 'http://' + url
+    return url
+
 # Função para raspar dados com base no URL e no nome do brinquedo
 def scrape_product_data(url, nome_brinquedo):
     try:
+        # Corrige o URL se necessário
+        url_corrigido = corrigir_url(url)
+
         # Faça uma requisição para o site fornecido
-        response = requests.get(url)
+        response = requests.get(url_corrigido)
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # Encontre os elementos que contêm as informações do produto
-        # Substitua 'seletor_css_do_produto' pelo seletor CSS correto
+        # Substitua 'seletor_css_do_produto' e 'seletor_css_do_preco' pelos seletores corretos
         product_element = soup.select_one('seletor_css_do_produto')
         descricao_produto = product_element.text.strip() if product_element else 'Não encontrado'
 
-        # Substitua 'seletor_css_do_preco' pelo seletor CSS correto
         preco_element = soup.select_one('seletor_css_do_preco')
         preco = preco_element.text.strip() if preco_element else 'Não encontrado'
 
